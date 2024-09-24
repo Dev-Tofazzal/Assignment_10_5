6783e3265e0c47e3779434bbc56e858db000c482
mdtofazzalislam <dev.tofazzal122@gmail.com>
2024-09-24 21:12:11 +0600

    faq correction

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
 document.getElementById("blog_btn").addEventListener("click",function(){
-    window.location.href = "/blog.html";
+    window.location.href = "/Assignment_10_5/blog.html";
 })
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.getElementById("home_btn").addEventListener("click",function(){
-    window.location.href= "/index.html"
+    window.location.href= "/Assignment_10_5/index.html"
     
     
 })
